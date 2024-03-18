42577e674caabb2ef06621dc32a9e12f38821bfd
ar12-snack <102972417+ar12-snack@users.noreply.github.com>
2024-03-18 12:31:45 -0400

    Added login page

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,7 +1,23 @@
-const Login = () => {
-    return(
-        <h1>Welcome</h1>
-    );
+import React from 'react';
+import '../App.css';
+import SquareWithImageAndForm from '../components/SquareWithImageAndForm'; // Import the component
+
+const login = () => {
+  const handleSubmit = () => {
+    // Handle form submission logic here
+  };
+
+  return (
+    <div>
+      <h1>Sign In Here</h1>
+      <SquareWithImageAndForm 
+        imageUrl= '../images/School.png'
+        labelText="Enter UUID to continue"
+        onSubmit={handleSubmit}
+        logoUrl='/logo.jpg'
+      />
+    </div>
+  );
 };
 
-export default Login;
+export default login;
