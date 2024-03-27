7f98fcc26f9f0a90d0b9e8cb4592c7cc5b49da74
NearbyBoris <124942714+NearbyBoris@users.noreply.github.com>
2024-03-27 12:05:08 -0400

    changes

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../App.css';
 import SquareWithImageAndForm from '../components/SquareWithImageAndForm'; // Import the component
+import { Link } from 'react-router-dom';
 
 const login = () => {
   const handleSubmit = () => {
     // Handle form submission logic here
+    <Link to={'/InstructorPage'}>
+    </Link>
   };
 
   return (
