96fd2049ce6d409758b6688716ef5143e8628783
Logan Bachman <lbachman@csci.northeaststate.edu>
2024-03-27 13:11:26 -0400

    added page for registration.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './routes';
 import Login from './routes/login';
+import Register from './routes/register';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route index element={<Index />} />
           <Route path='/login' element={<Login />} />
+          <Route path= '/register' element={<Register/>}/>
         </Routes>
       </Router>
     </div>
