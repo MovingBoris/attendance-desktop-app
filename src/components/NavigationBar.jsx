8180d2d44fa3659a5bee5ca56369bf997d3277dd
zhangw13 <128410552+zhangw13@users.noreply.github.com>
2024-04-03 11:10:33 -0400

    Update navigation bar design
    Components can be modified at any time based on future progress

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,25 +3,49 @@ import './Navbar.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
 
-  return (
-    <nav className="navbar">
-      <div className="navbar-logo">Attendance</div>
-      <ul className={`navbar-links ${isOpen ? "active" : ""}`}>
-        <Link to="/">
-          <li><a href="#">Home</a></li>
-        </Link>
-      </ul>
-      <div className="navbar-toggle" onClick={toggleNavbar}>
-        <i className={`fas ${isOpen ? "fa-times" : "fa-bars"}`}></i>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src={'/images/AttendanceLogo.jpg'} alt="Logo" className="logo-image" />
+                <div className="separator"></div> {/* Add separator bar */}
+                <div className="dropdown">
+                    <button className="dropbtn"> NESCC Attendance</button>
+                    <div className="dropdown-content">
+                        <Link to="/">Mark Attendance</Link>
+                        <Link to="/">View Attendance</Link>
+                        <Link to="/">Attendance Reports</Link>
+                    </div>
+                </div>
+            </div>
+            <ul className={`navbar-links ${isOpen ? "active" : ""}`}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/">Students</Link>
+                </li>
+                <li>
+                    <Link to="/">Courses</Link>
+                </li>
+                <li>
+                    <Link to="/">Instructors</Link>
+                </li>
+                <li>
+                    <Link to="/">Settings</Link>
+                    
+                </li>
+            </ul>
+            <div className="navbar-toggle" onClick={toggleNavbar}>
+                <i className={`fas ${isOpen ? "fa-times" : "fa-bars"}`}></i>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
